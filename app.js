01445d5de6cefa3cb5eb25502ec8e9d646d80795
Aidar <97720321+Aidar912@users.noreply.github.com>
2024-03-02 07:25:34 +0600

    cors upd

--- app.js
+++ app.js
@@ -44,6 +44,12 @@ app.use('/uploads', express.static('uploads'));
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 routes(app)
 
 app.listen(PORT, IP_ADDRESS,() => {
